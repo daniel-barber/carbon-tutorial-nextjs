f39440eeca390c5fcc144927ef3b4c0d30a70473
daniel-barber <daniel_barber23@hotmail.com>
2025-06-12 12:13:02 +0200

    ci: add GitHub Actions workflow

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // ← still needed for static-export
   output: 'export',
+
+  // ← where to emit your built & exported files
+  // this will create ./docs/index.html, ./docs/_next/, etc.
+  distDir: 'docs',
 };
 
 module.exports = nextConfig;
